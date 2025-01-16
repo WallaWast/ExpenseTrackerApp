9f4b728aa35801dedc46985517d964d4dd4e0674
Wallace Torres <Wallace.DaSilvaTorres@campus-support.ca>
2025-01-15 16:45:44 -0800

    Update expense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -10,6 +10,8 @@ const ManageExpense = ({ route, navigation }) => {
 	const editedExpenseId = route.params?.expenseId;
 	const isEditing = !!editedExpenseId;
 
+	const selectedExpense = expensesCtx.expenses.find((expense) => expense.id === editedExpenseId);
+
 	useLayoutEffect(() => {
 		navigation.setOptions({
 			title: isEditing ? 'Edit Expense' : 'Add Expense',
@@ -40,6 +42,7 @@ const ManageExpense = ({ route, navigation }) => {
 				onCancel={cancelHandler}
 				onSubmit={confirmHandler}
 				submitButtonLabel={isEditing ? 'Update' : 'Add'}
+				defaultValues={selectedExpense}
 			/>
 
 			{isEditing && (
--- util/date.js
+++ util/date.js
@@ -1,9 +1,5 @@
 export function getFormattedDate(date) {
-	const year = date.getFullYear();
-	const month = String(date.getMonth() + 1).padStart(2, '0'); // Ensures two digits for month
-	const day = String(date.getDate()).padStart(2, '0'); // Ensures two digits for day
-
-	return `${year}-${month}-${day}`;
+	return date.toISOString().slice(0, 10);
 }
 
 export function getDateMinusDays(date, days) {
